c840a30fbfe62cf4776824f00e8f13df05248273
Wouter van Atteveldt <wouter@vanatteveldt.com>
2019-01-08 15:26:18 +0100

    First post

---
+++ ojs.R
@@ -0,0 +1,51 @@
+#install.packages("RMySQL")
+library(RMySQL)
+library(tidyverse)
+
+# setup ssh tunnel
+# ssh -L 3307:localhost:3306 computationalcommunication.org, or if you're cool:
+system2("ssh", c("-L 3307:localhost:3306", "-N", "-T", "computationalcommunication.org"), wait=FALSE)
+
+# connect
+user = Sys.getenv("LOGNAME")
+conn = dbConnect(MySQL(), user=user, password=.rs.askForPassword("Enter password:"), dbname='ojs2', host='127.0.0.1', port=3307)
+# helper function to read full table as tibble
+t = function(...) dbReadTable(conn, ...) %>% as.tibble
+
+# informational: list tables / columns / data:
+dbListTables(conn) %>% (function(x) x[grep("subm", x)])
+dbListFields(conn, 'review_assignments')
+t('edit_decisions') 
+
+# submissions
+submissions = t('submissions') %>% mutate(date_submitted=as.Date(date_submitted)) %>% select(submission_id, date_submitted, status)
+titles = t('submission_settings') %>% filter(setting_name=='cleanTitle') %>% select(submission_id, setting_value)
+
+# decisions
+last_decision = t('edit_decisions') %>%  mutate(date_decided=as.Date(date_decided)) %>% 
+  group_by(submission_id) %>% arrange(desc(date_decided), desc(edit_decision_id)) %>% filter(row_number() == 1) %>%
+  left_join(t('users') %>% select(editor_id=user_id, editor=first_name)) %>% select(submission_id, editor, decision) 
+
+
+
+today = Sys.Date()
+longago = Sys.Date() - 14
+reviews = t('review_assignments') %>% select(submission_id, review_round_id, date_assigned, date_confirmed, date_completed, date_response_due, date_due, declined, round, step) %>%
+  mutate(date_assigned=as.Date(date_assigned), date_due=as.Date(date_due), date_confirmed=as.Date(date_confirmed), date_response_due=as.Date(date_response_due))
+reviews = reviews %>% mutate(status=ifelse(declined==1, "DECLINED", ifelse(is.na(date_confirmed), 
+                                                                           ifelse(date_response_due<today, ifelse(date_response_due<longago, "PENDING_WAY_OVERDUE", "PENDING_OVERDUE"), "PENDING"), 
+                                                                           ifelse(!is.na(date_completed), "COMPLETE", 
+                                                                                  ifelse(date_due<today, ifelse(date_due<longago, "WAY_OVERDUE",  "OVERDUE"),  "INPROGRESS")))))
+
+# count reivews per status per article
+review_status = reviews %>% group_by(submission_id, round, status) %>% summarize(n=n()) %>% spread(key=status, value=n, fill = 0) %>% 
+  mutate(NREVIEW=COMPLETE+INPROGRESS+OVERDUE+PENDING_OVERDUE+PENDING_WAY_OVERDUE)
+# select only most recent round per article
+review_status = review_status %>% group_by(submission_id) %>% arrange(-round) %>% filter(row_number() == 1) 
+
+known_decisions = tibble(decision=c(8,9), decision_name=c("Review", "Reject"))
+
+# join em all
+report = submissions %>% left_join(titles) %>% left_join(last_decision) %>% left_join(known_decisions) %>% left_join(review_status) %>% arrange(editor, submission_id)
+# problems
+report %>% filter(is.na(editor) | NREVIEW<2 | PENDING_WAY_OVERDUE>0)
